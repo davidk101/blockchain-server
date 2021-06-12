26c25ed623c0256b0cd3ad39073147c1e25e33da
David Kumar <david.dn.kumar@gmail.com>
2021-06-12 18:03:09 -0500

    implement pow algorithm on each block

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,10 +1,5 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
-
 // blockchain - public DB that doesn't rely on trust between nodes of data
 // here, blockchain nodes could produce incorrect/corrupted data and the DB can fix itself
 
@@ -16,8 +11,10 @@ type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+	Nonce    int
 }
 
+/* unnecessary now since hash derived in pow
 // takes in a pointer to a block as a parameter
 func (b *Block) DeriveHash() {
 
@@ -26,12 +23,20 @@ func (b *Block) DeriveHash() {
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
 }
+*/
 
 // outputs a pointer to a block
 func CreateBlock(data string, prevHash []byte) *Block {
 
-	block := &Block{[]byte{}, []byte(data), prevHash} // this is a reference to Block
-	block.DeriveHash()
+	block := &Block{[]byte{}, []byte(data), prevHash, 0} // this is a reference to Block
+
+	// run pow algorithm on each block
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
